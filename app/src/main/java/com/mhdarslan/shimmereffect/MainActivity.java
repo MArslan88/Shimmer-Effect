f52489b7e0d5e1c7f6285cc3f3b1be9feff77c97
MArslan88 <gery_maz_555@hotmail.com>
2023-06-27 11:25:49 +0500

    include both layouts in MainActivity, and apply the shimmer effect

--- app/src/main/java/com/mhdarslan/shimmereffect/MainActivity.java
+++ app/src/main/java/com/mhdarslan/shimmereffect/MainActivity.java
@@ -3,12 +3,34 @@ package com.mhdarslan.shimmereffect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    ShimmerFrameLayout shimmerFrameLayout;
+    LinearLayout dataLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        shimmerFrameLayout = findViewById(R.id.shimmer_view);
+        dataLayout = findViewById(R.id.data_view);
+
+
+        dataLayout.setVisibility(View.INVISIBLE);
+        shimmerFrameLayout.startShimmer();
+
+        Handler handler = new Handler();
+        handler.postDelayed(()->{
+            dataLayout.setVisibility(View.VISIBLE);
+            shimmerFrameLayout.stopShimmer();
+            shimmerFrameLayout.setVisibility(View.INVISIBLE);
+        }, 5000);
     }
 }
